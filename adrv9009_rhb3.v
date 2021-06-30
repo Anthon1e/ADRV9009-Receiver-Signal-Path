f800e055f0a492d3fc097ddc27e6a07e6a77c5e9
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-06-30 17:26:49 +0700

    Fix timing issues by removing array/ram block

--- adrv9009_rhb3.v
+++ adrv9009_rhb3.v
@@ -28,61 +28,72 @@ module adrv9009_rhb3(
     );
     
     // RHB3 filter coefficient, MSB is signed bit
-    wire signed [15:0] coeff [0:8];
-    assign coeff[0] = 16'hfd9a; // (-0.01874) * 2^15 = -614
-    assign coeff[1] = 16'hfa9a; // (?0.04218) * 2^15 = -1382
-    assign coeff[2] = 16'h0676; // 0.050476 * 2^15 = 1654
-    assign coeff[3] = 16'h259e; // 0.293884 * 2^15 = 9630
-    assign coeff[4] = 16'h3846; // 0.439636 * 2^15 = 14406
-    assign coeff[5] = 16'h259e; // 0.293884 * 2^15 = 9630
-    assign coeff[6] = 16'h0676; // 0.050476 * 2^15 = 1654
-    assign coeff[7] = 16'hfa9a; // (-0.04218) * 2^15 = -1382
-    assign coeff[8] = 16'hfd9a; // (-0.01874) * 2^15 = -614
+    wire signed [15:0] coeff0, coeff1, coeff2, coeff3, coeff4, coeff5, coeff6, coeff7, coeff8;
+    assign coeff0 = 16'hfd9a; // (-0.01874) * 2^15 = -614
+    assign coeff1 = 16'hfa9a; // (?0.04218) * 2^15 = -1382
+    assign coeff2 = 16'h0676; // 0.050476 * 2^15 = 1654
+    assign coeff3 = 16'h259e; // 0.293884 * 2^15 = 9630
+    assign coeff4 = 16'h3846; // 0.439636 * 2^15 = 14406
+    assign coeff5 = 16'h259e; // 0.293884 * 2^15 = 9630
+    assign coeff6 = 16'h0676; // 0.050476 * 2^15 = 1654
+    assign coeff7 = 16'hfa9a; // (-0.04218) * 2^15 = -1382
+    assign coeff8 = 16'hfd9a; // (-0.01874) * 2^15 = -614
     
     // Save previous input data
-    reg signed [15:0] zin [0:7];
-    genvar i;
-    generate
-    for (i = 0; i < 8; i = i + 1) begin
-        always @(posedge clk) begin
-            if (reset) begin
-                zin[i] <= 16'b0; 
-            end else begin
-                if (i > 0) 
-                    zin[i] <= zin[i-1];
-                if (i == 0) 
-                    zin[i] <= in;
-            end
+    reg signed [15:0] zin1, zin2, zin3, zin4, zin5, zin6, zin7, zin8;
+    always @(posedge clk) begin
+        if (reset) begin
+            {zin1, zin2, zin3, zin4, zin5, zin6, zin7, zin8} <= {8{16'b0}};
+        end else begin
+            zin1 <= in;
+            {zin2, zin3, zin4, zin5, zin6, zin7, zin8} <= {zin1, zin2, zin3, zin4, zin5, zin6, zin7};
         end
     end
-    endgenerate
     
     // Coefficient multiplation
-    reg signed [31:0] xtimesh [0:8];
-    genvar j;
-    generate
-    for (j = 0; j < 9; j = j + 1) begin
-        always @(posedge clk) begin
-            if (reset) begin
-                xtimesh[j] <= 32'b0;
-            end else begin
-                if (j > 0)
-                    xtimesh[j] <= coeff[j] * zin[j-1];
-                else 
-                    xtimesh[j] <= coeff[j] * in;
-            end
+    reg signed [31:0] xh0, xh1, xh2, xh3, xh4, xh5, xh6, xh7, xh8;
+    always @(posedge clk) begin
+        if (reset) begin
+            {xh0, xh1, xh2, xh3, xh4, xh5, xh6, xh7, xh8} <= {9{32'b0}};
+        end else begin
+            xh0 <= coeff0 * in;
+            xh1 <= coeff1 * zin1;
+            xh2 <= coeff2 * zin2;
+            xh3 <= coeff3 * zin3;
+            xh4 <= coeff4 * zin4;
+            xh5 <= coeff5 * zin5;
+            xh6 <= coeff6 * zin6;
+            xh7 <= coeff7 * zin7;
+            xh8 <= coeff8 * zin8;
         end
     end
-    endgenerate
     
     // Output
-    integer k;
-    /*always @(*) begin
-        out = 32'b0;
-        for (k = 0; k < 9; k = k + 1) begin
-            out = out + xtimesh[k];
+    reg signed [31:0] xxh0, xxh1, xxh2, xxh3, xxh4, xxh5, xxh6, xxh7, xxh8;
+    reg signed [64:0] out1, out2, out3, out4, out5, out6, out7, out8, out9, out0;
+    always @(posedge clk) begin
+        if (reset) begin
+            out <= 32'b0;
+            {xxh0, xxh1, xxh2, xxh3, xxh4, xxh5, xxh6, xxh7, xxh8} <= {9{32'b0}};
+            {out0, out1, out2, out3, out4, out5, out6, out7, out8, out9} <= {10{32'b0}};
+        end else begin 
+            // Cycle delay 1: Allow for multiplication process
+            {xxh0, xxh1, xxh2, xxh3, xxh4, xxh5, xxh6, xxh7, xxh8} <= {xh0, xh1, xh2, xh3, xh4, xh5, xh6, xh7, xh8};
+            // Cycle delay 2: Allow for summation process
+            out1 <= xxh0 + xxh1;
+            out2 <= xxh2 + xxh3;
+            out3 <= xxh4 + xxh5;
+            out4 <= xxh6 + xxh7;
+            out5 <= xxh8;
+            // Cycle delay 3
+            out6 <= out1 + out2;
+            out7 <= out3 + out4;
+            out8 <= out5;
+            // Cycle delay 4
+            out9 <= out6 + out7;
+            out0 <= out8;
+            // Finally output
+            out <= out9 + out0;
         end
-    end*/
-    always @(posedge clk) 
-        out <= xtimesh[0] + xtimesh[1] + xtimesh[2] + xtimesh[3] + xtimesh[4] + xtimesh[5] + xtimesh[6] + xtimesh[7] + xtimesh[8];
+    end
 endmodule
