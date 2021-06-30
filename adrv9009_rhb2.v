fe4ae57c47fc44c6e12d72b2c4c86eef5701f459
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-06-30 14:13:40 +0700

    Create adrv9009_rhb2.v

---
+++ adrv9009_rhb2.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/29/2021 08:16:45 AM
+// Design Name: 
+// Module Name: adrv9009_rhb2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adrv9009_rhb2(
+    input clk,
+    input reset,
+    input signed [31:0] in,
+    output signed [31:0] out
+    );
+    
+    assign out = in;
+endmodule
