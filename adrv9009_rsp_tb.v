c2325f3ca484a3ae1b1f441ff7a86bd45cb58d2f
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-07-01 11:55:34 +0700

    Add 2-wave input testbench

--- adrv9009_rsp_tb.v
+++ adrv9009_rsp_tb.v
@@ -21,11 +21,12 @@
 
 
 module adrv9009_rsp_tb #(
-    parameter period = 9 // clock cycles
+    parameter period1 = 9, 
+    parameter period2 = 4 
     ) ();
     reg clk, reset;
     reg signed [15:0] in;
-    wire signed [31:0] out;
+    wire signed [15:0] out;
     
     adrv9009_rsp DUT (
     .clk (clk),
@@ -34,7 +35,7 @@ module adrv9009_rsp_tb #(
     .out (out)
     );
 
-    // Set up for the rise of clock every 2 seconds
+    // Set up for the rise of clock every 10ns (100MHz)
     initial begin
         clk <= 1'b1;
         #1;
@@ -66,56 +67,134 @@ module adrv9009_rsp_tb #(
     `define STATE8 4'b1000
     `define STATE9 4'b1001 
     
-    reg [3:0] cs;
-    reg [3:0] count;
+    // First sin wave
+    reg [3:0] cs1;
+    reg [3:0] count1;
+    reg signed [15:0] in1;
     always @(posedge clk) begin
         if (reset) begin
-            in <= 16'b0;
-            cs <= `STATE0;
-            count <= 0;  
+            in1 <= 16'b0;
+            cs1 <= `STATE0;
+            count1 <= 0;  
         end else begin
-            case (cs)
+            case (cs1)
                 `STATE0: begin 
-                    in <= 0;
-                    if (count < period) begin cs <= `STATE0; count <= count + 1; end 
-                    else                begin cs <= `STATE1; count <= 0; end                            
+                    in1 <= 0;
+                    if (count1 < period1)   begin cs1 <= `STATE0; count1 <= count1 + 1; end 
+                    else                    begin cs1 <= `STATE1; count1 <= 0; end                            
                 end
                 `STATE1: begin
-                    in <= 23170;
-                    if (count < period) begin cs <= `STATE1; count <= count + 1; end   
-                    else                begin cs <= `STATE2; count <= 0; end           
+                    in1 <= 23170;
+                    if (count1 < period1)   begin cs1 <= `STATE1; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE2; count1 <= 0; end           
                 end
                 `STATE2: begin
-                    in <= 32767;
-                    if (count < period) begin cs <= `STATE2; count <= count + 1; end   
-                    else                begin cs <= `STATE3; count <= 0; end           
+                    in1 <= 32767;
+                    if (count1 < period1)   begin cs1 <= `STATE2; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE3; count1 <= 0; end           
                 end
                 `STATE3: begin
-                    in <= 23170;
-                    if (count < period) begin cs <= `STATE3; count <= count + 1; end   
-                    else                begin cs <= `STATE4; count <= 0; end           
+                    in1 <= 23170;
+                    if (count1 < period1)   begin cs1 <= `STATE3; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE4; count1 <= 0; end           
                 end
                 `STATE4: begin
-                    in <= 0;
-                    if (count < period) begin cs <= `STATE4; count <= count + 1; end   
-                    else                begin cs <= `STATE5; count <= 0; end           
+                    in1 <= 0;
+                    if (count1 < period1)   begin cs1 <= `STATE4; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE5; count1 <= 0; end           
                 end
                 `STATE5: begin
-                    in <= -23170;
-                    if (count < period) begin cs <= `STATE5; count <= count + 1; end   
-                    else                begin cs <= `STATE6; count <= 0; end           
+                    in1 <= -23170;
+                    if (count1 < period1)   begin cs1 <= `STATE5; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE6; count1 <= 0; end           
                 end
                 `STATE6: begin
-                    in <= -32768;
-                    if (count < period) begin cs <= `STATE6; count <= count + 1; end   
-                    else                begin cs <= `STATE7; count <= 0; end           
+                    in1 <= -32768;
+                    if (count1 < period1)   begin cs1 <= `STATE6; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE7; count1 <= 0; end           
                 end
                 `STATE7: begin
-                    in <= -23170;
-                    if (count < period) begin cs <= `STATE7; count <= count + 1; end   
-                    else                begin cs <= `STATE0; count <= 0; end           
+                    in1 <= -23170;
+                    if (count1 < period1)   begin cs1 <= `STATE7; count1 <= count1 + 1; end   
+                    else                    begin cs1 <= `STATE0; count1 <= 0; end           
                 end
             endcase
+        end
+    end
+    
+    // Second sin wave
+    reg [3:0] cs2;
+    reg [3:0] count2;
+    reg signed [15:0] in2;
+    always @(posedge clk) begin
+        if (reset) begin
+            in2 <= 16'b0;
+            cs2 <= `STATE0;
+            count2 <= 0;  
+        end else begin
+            case (cs2)
+                `STATE0: begin 
+                    in2 <= 0;
+                    if (count2 < period2)   begin cs2 <= `STATE0; count2 <= count2 + 1; end 
+                    else                    begin cs2 <= `STATE1; count2 <= 0; end                            
+                end
+                `STATE1: begin
+                    in2 <= 23170;
+                    if (count2 < period2)   begin cs2 <= `STATE1; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE2; count2 <= 0; end           
+                end
+                `STATE2: begin
+                    in2 <= 32767;
+                    if (count2 < period2)   begin cs2 <= `STATE2; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE3; count2 <= 0; end           
+                end
+                `STATE3: begin
+                    in2 <= 23170;
+                    if (count2 < period2)   begin cs2 <= `STATE3; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE4; count2 <= 0; end           
+                end
+                `STATE4: begin
+                    in2 <= 0;
+                    if (count2 < period2)   begin cs2 <= `STATE4; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE5; count2 <= 0; end           
+                end
+                `STATE5: begin
+                    in2 <= -23170;
+                    if (count2 < period2)   begin cs2 <= `STATE5; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE6; count2 <= 0; end           
+                end
+                `STATE6: begin
+                    in2 <= -32768;
+                    if (count2 < period2)   begin cs2 <= `STATE6; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE7; count2 <= 0; end           
+                end
+                `STATE7: begin
+                    in2 <= -23170;
+                    if (count2 < period2)   begin cs2 <= `STATE7; count2 <= count2 + 1; end   
+                    else                    begin cs2 <= `STATE0; count2 <= 0; end           
+                end
+            endcase
+        end
+    end
+    
+    // Choosing between 2 inputs
+    reg [12:0] counter;
+    always @(posedge clk) begin
+        if (reset) begin 
+            in <= 16'b0;
+            counter <= 13'b0;
+        end else begin
+            if (counter < 15) begin
+                counter <= counter + 1;
+            end else if (counter < 300) begin
+                counter <= counter + 1;
+                in <= in1;
+            end else if (counter < 600) begin
+                counter <= counter + 1;
+                in <= in2;
+            end else begin
+                in <= in1 + in2;
+            end
         end
     end
 endmodule
