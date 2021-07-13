142e2d54ef297d70dfce34c6d80b65e104341003
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-07-13 10:45:22 +0700

    Add RAM module and programmable RFIR

--- adrv9009_rsp.v
+++ adrv9009_rsp.v
@@ -21,16 +21,32 @@
 
 
 module adrv9009_rsp (
-    input clk,
+    // General stuff
+    input clk_m,
     input reset,
     input signed [15:0] in,
-    output signed [15:0] out
+    output signed [15:0] out,
+    // For rfir module
+    input en_rfir,
+    input [1:0] mode_rfir,
+    // For RAM module
+    input clk_r,
+    input wr_en,
+    input [6:0] addr_in,
+    input signed [15:0] coeff_in
     );
     
-    wire [15:0] out_rhb3, out_rhb2;
+    wire signed [15:0] out_rhb3, out_rhb2, coeff_out;
+    wire [6:0] addr_out;
     
-    adrv9009_rhb3 rhb3 (.clk (clk), .reset (reset), .in (in), .out (out_rhb3));
-    adrv9009_rhb2 rhb2 (.clk (clk), .reset (reset), .in (out_rhb3), .out (out_rhb2)); 
-    adrv9009_rfir rfir (.clk (clk), .reset (reset), .in (out_rhb2), .out (out));
+    // ADRV9009 Receiver signal datapath
+    adrv9009_rhb3 rhb3 (.clk (clk_m), .reset (reset), .in (in), .out (out_rhb3));
+    adrv9009_rhb2 rhb2 (.clk (clk_m), .reset (reset), .in (out_rhb3), .out (out_rhb2));
+    adrv9009_rfir rfir (.clk (clk_m), .reset (reset), .en_rfir (en_rfir), .mode_rfir (mode_rfir), 
+                        .in (out_rhb2), .out (out), .ram_coeff (coeff_out), .addr_out (addr_out));
     
+    // RAM modules to hold coefficients 
+    RAM_coeff ramc (.clk_m (clk_m), .clk_r (clk_r), .wr_en (wr_en), .addr_in (addr_in), 
+                    .addr_out (addr_out), .in (coeff_in), .out (coeff_out)); 
+
 endmodule
